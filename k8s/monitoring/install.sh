e4d534dade02af699b5a2af90314b59bbb4b407c
Said Abdan Syakur <saidabdan12@gmail.com>
2026-04-20 12:29:09 +0700

    tambahin install.sh biar langsung kubectl apply grafana-ingress

--- k8s/monitoring/install.sh
+++ k8s/monitoring/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+set -e
 
 helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
   -n monitoring \
+  --create-namespace \
   -f values.yaml
+
+kubectl rollout status deployment/monitoring-grafana -n monitoring
+
+kubectl apply -f grafana-ingress.yaml
